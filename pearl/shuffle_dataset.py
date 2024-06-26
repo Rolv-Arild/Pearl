cecdb73cb961e849e5e07e9d284d579af8e8644b
Rolv-Arild <rolv_arild@hotmail.com>
2024-06-26 20:47:06 +0200

    Hopefully lower complexity of shuffling algorithm

--- pearl/shuffle_dataset.py
+++ pearl/shuffle_dataset.py
@@ -1,86 +1,98 @@
 import math
 import os
 import shutil
-import time
 from argparse import ArgumentParser
-from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
 from tqdm import tqdm
 
-from pearl.data import EpisodeData
+from pearl.data import EpisodeData, GameInfo
 
 
-def load_episode_data(file):
-    return EpisodeData.load(file)
+def load_episode_data(file, target_size):
+    if os.path.isfile(file):
+        return EpisodeData.load(file)
+    dummy = EpisodeData.new_empty(target_size, normalized=True)
+    dummy.game_info[:, GameInfo.IGNORE] = np.nan
+    return dummy
 
 
 def main(args):
     input_dir = args.input_dir
     output_dir = args.output_dir
+    shard_size = args.shard_size
 
     files = os.listdir(input_dir)  # Keep the last file as validation
     files = [os.path.join(input_dir, file) for file in files if file.endswith(".npz")]
     train_files = files[:-1]
     val_files = files[-1:]
-    # Get the dataset size first
-    sizes = []
-    pbar = tqdm(train_files, desc="Getting dataset size")
-    for file in pbar:
-        try:
-            shard = EpisodeData.load(file)
-        except Exception as e:
-            print(f"Error loading {file}: {e}")
-            raise e
-        sizes.append(len(shard))
-        pbar.set_postfix_str(f"{file}, size={len(shard)}")
-    total_samples = sum(sizes)
-    print(f"Total samples: {total_samples}")
-
-    # Make a global list of shuffled indices
-    indices = np.arange(total_samples)
-    np.random.shuffle(indices)
 
     # Now, we need to make new shards with globally shuffled data.
-    # This is O(n^2) but it only requires two shards in memory at a time.
-    target_size = math.ceil(total_samples / len(train_files))  # Make sure we don't have any nearly empty shards
-    current_shard = 0
-    current_index = 0
+    # We use a butterfly shuffle, so the complexity is O(n * log(n))
 
-    print("Shuffling dataset")
-    while current_index < total_samples:
-        new_shard = EpisodeData.new_empty(target_size, normalized=True)
-        new_shard.ball_data[:] = np.nan  # So we can check if we missed any samples at the end
-        new_to_old = indices[current_index:current_index + target_size]
-        # E.g. new_to_old[0] = 5 means that the first sample in the new shard is the 5th sample in all the old shards
+    # Initialize the outputs as the original files
+    for n, file in enumerate(train_files):
+        shutil.copy(file, os.path.join(output_dir, f"tmp_shard_{n}.npz"))
 
-        # Remove empty samples from the end so the indices match
-        new_shard = new_shard[:len(new_to_old)]
+    num_shards = len(train_files)
+    w = 1
+    lim = math.ceil(np.log2(num_shards))
+    swapped = [(n,) for n in range(lim)]
+    pbar = tqdm(list(range(lim)), "Shuffling shards")
+    for p in pbar:
+        # Note that we use ceil instead of floor, so we don't lose any data.
+        # This means we will encounter shards that don't exist, which we will fill with dummy data.
+        # The dummy data is removed later on
+        t = 0
+        while t < num_shards:
+            s = 0
+            while s < w:
+                a = s + t
+                b = a + w
+                pbar.set_postfix_str(f"Swapping {a} and {b}")
+                shard_a = load_episode_data(os.path.join(output_dir, f"tmp_shard_{a}.npz"), shard_size)
+                shard_b = load_episode_data(os.path.join(output_dir, f"tmp_shard_{b}.npz"), shard_size)
+                total_shard = shard_a + shard_b
+                total_shard.shuffle()
+                shard_a = total_shard[:len(total_shard) // 2]
+                shard_b = total_shard[len(total_shard) // 2:]
+                shard_a.save(os.path.join(output_dir, f"tmp_shard_{a}.npz"))
+                shard_b.save(os.path.join(output_dir, f"tmp_shard_{b}.npz"))
 
-        start_index = 0
-        # Parallelize
-        with ThreadPoolExecutor(1) as executor:
-            pbar = tqdm(train_files, desc=f"Shard {current_shard}")
-            exmap = executor.map(load_episode_data, train_files)
-            for n, (file, old_shard) in enumerate(zip(pbar, exmap)):
-                end_index = start_index + len(old_shard)
+                res: tuple = swapped[a] + swapped[b]
+                assert len(set(res)) == len(res), f"Duplicate shards in swap: {res}"
+                swapped[a] = res
+                swapped[b] = res
 
-                # Find the samples that are in the new shard
-                mask = np.logical_and(new_to_old >= start_index, new_to_old < end_index)
-                new_indices = new_to_old[mask] - start_index
-                new_shard[mask] = old_shard[new_indices]
+                s += 1
+            t += 2 * w
+        w *= 2
 
-                start_index = end_index
+    # Remove dummy data
+    n = 0
+    i = 0
+    out_shard = EpisodeData.new_empty(shard_size, normalized=True)
+    for shard_file in os.listdir(output_dir):
+        if not shard_file.startswith("tmp_shard_"):
+            continue
+        shard = EpisodeData.load(os.path.join(output_dir, shard_file))
+        shard = shard[np.isnan(shard.game_info[:, GameInfo.IGNORE])]
+        if i + len(shard) > shard_size:
+            out_shard[i:] = shard[:shard_size - i]
+            shard = shard[shard_size - i:]
+            out_shard.save(os.path.join(output_dir, f"training_shard_{n}.npz"))
+            n += 1
+            out_shard = EpisodeData.new_empty(shard_size, normalized=True)
+            i = 0
+        out_shard[i:i + len(shard)] = shard
+        i += len(shard)
+    if i > 0:
+        out_shard.save(os.path.join(output_dir, f"training_shard_{n}.npz"))
 
-            # Check if we missed any samples
-            if np.isnan(new_shard.ball_data).any():
-                debug = True
-                raise ValueError("Missed samples")
-
-            print(f"Saving shard {current_shard} with {len(new_shard)} samples")
-            new_shard.save(os.path.join(output_dir, f"train_shard_{current_shard}.npz"))
-            current_shard += 1
-            current_index += target_size
+    # Clean up any remaining intermediate files
+    for shard_file in os.listdir(output_dir):
+        if shard_file.startswith("tmp_shard_"):
+            os.remove(os.path.join(output_dir, shard_file))
 
     # Copy validation files
     for n, file in enumerate(val_files):
@@ -91,5 +103,6 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument("--input_dir", required=True)
     parser.add_argument("--output_dir", required=True)
+    parser.add_argument("--shard_size", type=int, default=30 * 60 * 60 * 24)
     args = parser.parse_args()
     main(args)
